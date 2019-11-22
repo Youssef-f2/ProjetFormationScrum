a64b3f823dbf1335b3c93f9e0f9ed4a2fe921290
Youssef-f2 <nhir.youssef@gmail.com>
2019-11-22 13:10:43 +0100

    Commit Projet

---
+++ ScrumProjet/Form_ListeFormations.aspx.designer.vb
@@ -0,0 +1,24 @@
+﻿'------------------------------------------------------------------------------
+' <généré automatiquement>
+'     Ce code a été généré par un outil.
+'
+'     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+'     le code est régénéré.
+' </généré automatiquement>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class Form_ListeFormations
+
+    '''<summary>
+    '''Contrôle form1.
+    '''</summary>
+    '''<remarks>
+    '''Champ généré automatiquement.
+    '''Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+End Class
